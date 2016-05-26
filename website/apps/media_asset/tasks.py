05dbf300d7984b5da4093d096db325d36ab57f34
Jonas Ohrstrom <ohrstrom@gmail.com>
2016-05-26 16:34:53 +0200

    refactored process_waveform & process_format to celery/async

---
+++ website/apps/media_asset/tasks.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+import logging
+import os
+from django.conf import settings
+from project.celery import app
+
+log = logging.getLogger(__name__)
+
+from .models import Waveform, Format
+
+@app.task
+def process_waveform(media, type):
+    Waveform.objects.get_or_create_for_media(media=media, type=type)
+
+@app.task
+def process_format(media, encoding, quality):
+    Waveform.objects.get_or_create_for_media(media=media, encoding=encoding, quality=quality)
