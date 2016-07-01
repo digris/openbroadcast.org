eda3d02d51028fa2cf305ec6c2fe0f74f0c72182
Jonas Ohrstrom <ohrstrom@gmail.com>
2016-07-01 17:15:14 +0200

    fixes #1458

--- website/apps/alibrary/util/api_compare.py
+++ website/apps/alibrary/util/api_compare.py
@@ -739,9 +739,7 @@ class DiscogsAPILookup(APILookup):
 
 
 
-"""
-function to call from 'outside'
-"""
+
 def get_from_provider(item_type, item_id, provider, api_url=None):
 
     log.debug('get_from_provider: %s - id: %s - provider: %s - %s' % (item_type, item_id, provider, api_url))
