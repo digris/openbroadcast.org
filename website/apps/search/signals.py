7dabe80f3f1469f9c930591a63b0c0f11a25df8d
Jonas Ohrstrom <ohrstrom@gmail.com>
2018-03-12 17:45:17 +0100

    minor cleanup

--- website/apps/search/signals.py
+++ website/apps/search/signals.py
@@ -1,14 +1,17 @@
-from django.db import models
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import logging
+
+from django.contrib.contenttypes.models import ContentType
 from haystack import signals
 from haystack.exceptions import NotHandled
-
 
 class SearchIndexProcessor(signals.RealtimeSignalProcessor):
 
 
 
     def handle_save(self, sender, instance, **kwargs):
-
         """
         Given an individual model instance, determine which backends the
         update should be sent to & update the object on those backends.
@@ -20,7 +23,7 @@ class SearchIndexProcessor(signals.RealtimeSignalProcessor):
                 index = self.connections[using].get_unified_index().get_index(sender)
                 index.update_object(instance, using=using)
 
-                print 'updated index for {}'.format(instance)
+                print('updated index for {}'.format(instance))
 
             except NotHandled as e:
                 pass
--- website/project/wsgi.py
+++ website/project/wsgi.py
@@ -1,8 +1,7 @@
 import os
-from raven.contrib.django.raven_compat.middleware.wsgi import Sentry
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 
 application = get_wsgi_application()
-#application = Sentry(get_wsgi_application())
