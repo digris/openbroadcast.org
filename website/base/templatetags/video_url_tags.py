37e0f8d5766a9983566d82657792459f194c9c10
Jonas Ohrstrom <ohrstrom@gmail.com>
2018-06-09 14:36:54 +0200

    cleanup legacy templatetags

--- website/base/templatetags/video_url_tags.py
+++ website/base/templatetags/video_url_tags.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
 import re
-
 from django import template
+
 register = template.Library()
 
-@register.inclusion_tag('lib/templatetags/video_by_url.html', takes_context=True)
+@register.inclusion_tag('base/templatetags/video_by_url.html', takes_context=True)
 def video_by_url(context, url):
     context.update({'video_id': extract_video_id(url)})
     return context
-
 
 def extract_video_id(video_url):
     m = re.search(r"youtube\.com/.*v=([^&]*)", '%s' % video_url)
