e167bb2330adc956a0b0de9f3194d7e5b035ffb3
Jonas Ohrstrom <ohrstrom@gmail.com>
2018-06-11 12:18:49 +0200

    file cleanup

--- website/apps/abcast/views/stationviews.py
+++ website/apps/abcast/views/stationviews.py
@@ -12,10 +12,10 @@ PAGINATE_BY_DEFAULT = getattr(settings, 'AGINATE_BY_DEFAULT', 12)
 
 
 class StationListView(PaginationMixin, ListView):
-    
+
     object = Station
     paginate_by = PAGINATE_BY_DEFAULT
-    
+
     model = Station
     extra_context = {}
 
@@ -24,20 +24,20 @@ class StationListView(PaginationMixin, ListView):
         self.relation_filter = []
 
     def get_paginate_by(self, queryset):
-        
+
         ipp = self.request.GET.get('ipp', None)
         if ipp:
             try:
                 if int(ipp) in PAGINATE_BY:
                     return int(ipp)
-            except Exception, e:
+            except Exception as e:
                 pass
 
         return PAGINATE_BY_DEFAULT
 
     def get_context_data(self, **kwargs):
         context = super(StationListView, self).get_context_data(**kwargs)
-        
+
         self.extra_context['filter'] = self.filter
         self.extra_context['relation_filter'] = self.relation_filter
         self.extra_context['tagcloud'] = self.tagcloud
@@ -46,7 +46,7 @@ class StationListView(PaginationMixin, ListView):
         context.update(self.extra_context)
 
         return context
-    
+
 
     def get_queryset(self, **kwargs):
 
@@ -55,43 +55,27 @@ class StationListView(PaginationMixin, ListView):
         self.tagcloud = None
 
         q = self.request.GET.get('q', None)
-        
+
         if q:
             qs = Station.objects.filter(Q(name__istartswith=q))\
             .distinct()
         else:
             qs = Station.objects.all()
-            
-            
+
+
         order_by = self.request.GET.get('order_by', None)
         direction = self.request.GET.get('direction', None)
-        
+
         if order_by and direction:
             if direction == 'descending':
                 qs = qs.order_by('-%s' % order_by)
             else:
                 qs = qs.order_by('%s' % order_by)
-            
-            
-            
-        # special relation filters
 
-
-
-
-
-        # base queryset        
-        #qs = Release.objects.all()
-        
         # apply filters
         self.filter = StationFilter(self.request.GET, queryset=qs)
-        # self.filter = ReleaseFilter(self.request.GET, queryset=Release.objects.active().filter(**kwargs))
-        
         qs = self.filter.qs
-        
-        
-        
-        
+
         stags = self.request.GET.get('tags', None)
         tstags = []
         if stags:
@@ -99,18 +83,15 @@ class StationListView(PaginationMixin, ListView):
             for stag in stags:
                 tstags.append(int(stag))
 
-            
-            
         # rebuild filter after applying tags
         self.filter = StationFilter(self.request.GET, queryset=qs)
-        
+
         # tagging / cloud generation
         tagcloud = Tag.objects.usage_for_queryset(qs, counts=True, min_count=0)
 
         self.tagcloud = calculate_cloud(tagcloud)
-        
-        return qs
 
+        return qs
 
 
 class StationDetailView(DetailView):
@@ -121,19 +102,12 @@ class StationDetailView(DetailView):
 
     def render_to_response(self, context, **kwargs):
         return super(StationDetailView, self).render_to_response(context, content_type="text/html")
-    
 
-        
     def get_context_data(self, **kwargs):
-        
+
         obj = kwargs.get('object', None)
         context = super(StationDetailView, self).get_context_data(**kwargs)
-
-        members = obj.members.all()
-        self.extra_context['members'] = members
-
+        self.extra_context['members'] = obj.members.all()
         context.update(self.extra_context)
 
         return context
-
-    
