ecdb00f6e72b8c2bb7a03d5fb483b2d95ce630a0
Jonas Ohrstrom <ohrstrom@gmail.com>
2019-02-14 17:53:38 +0100

    implemented label statistics (db index migration)

---
+++ website/apps/atracker/migrations/0002_added_object_pk_index.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('atracker', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='event',
+            name='object_id',
+            field=models.PositiveIntegerField(db_index=True, null=True, blank=True),
+        ),
+    ]
