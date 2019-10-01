74e3568e3dbf89d22539753edda821cac0f6d9d6
Jonas Ohrstrom <ohrstrom@gmail.com>
2019-10-01 13:32:14 +0200

    implemented `ManifestStaticFilesStorage`

--- website/project/settings/components/20-media.py
+++ website/project/settings/components/20-media.py
@@ -1,33 +1,29 @@
 # -*- coding: utf-8 -*-
 import os
 from django.conf import settings
-BASE_DIR = getattr(settings, 'BASE_DIR')
 
+BASE_DIR = getattr(settings, "BASE_DIR")
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static-dist')
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static-dist")
+STATIC_URL = "/static/"
 
 ADMIN_MEDIA_PREFIX = os.path.join(STATIC_URL, "admin/")
-ADMIN_MEDIA_PREFIX = '/static/admin/'
+ADMIN_MEDIA_PREFIX = "/static/admin/"
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static-src'),
-)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static-src"),)
 
 STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    'compressor.finders.CompressorFinder',
-    'dajaxice.finders.DajaxiceFinder',
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+    "compressor.finders.CompressorFinder",
+    "dajaxice.finders.DajaxiceFinder",
 )
 
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
 
-FIXTURE_DIRS = [
-    os.path.join(BASE_DIR, 'fixtures'),
-]
+FIXTURE_DIRS = [os.path.join(BASE_DIR, "fixtures")]
 
 LEGACY_STORAGE_ROOT = None
-
