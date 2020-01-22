c22676cebec901e87c1872c6a6b389e27a6abfbf
Jonas Ohrstrom <ohrstrom@gmail.com>
2020-01-22 13:51:08 +0100

    cleanup & ci config

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # open broadcast - platform
 
-
+[![Actions Status](https://github.com/{owner}/{repo}/workflows/{workflow_name}/badge.svg)](https://github.com/{owner}/{repo}/actions)
 
--- cloc.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-cloc --exclude-dir="site-static" website static
-
-
-# git ls-files | while read f; do git blame --line-porcelain $f | grep '^author '; done | sort -f | uniq -ic | sort -n
