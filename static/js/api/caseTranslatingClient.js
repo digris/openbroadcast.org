b54e1db4176ffd842f1e8af29ea26f277d6c0565
Jonas Ohrstrom <ohrstrom@gmail.com>
2021-03-11 18:42:46 +0100

    Implemented throttling for API requests (axios).

--- static/js/api/caseTranslatingClient.js
+++ static/js/api/caseTranslatingClient.js
@@ -1,6 +1,10 @@
 import applyConverters from 'axios-case-converter';
 import axios from 'axios';
 
+const MAX_REQUESTS_COUNT = 4;
+const INTERVAL_MS = 10;
+let PENDING_REQUESTS = 0;
+
 const APIClient = applyConverters(axios.create({
   xsrfHeaderName: 'X-CSRFTOKEN',
   xsrfCookieName: 'csrftoken',
@@ -9,5 +13,44 @@ const APIClient = applyConverters(axios.create({
     'X-Requested-With': 'XMLHttpRequest',
   },
 }));
+
+/**
+ * Axios Request Interceptor
+ */
+APIClient.interceptors.request.use((config) => new Promise((resolve) => {
+    const interval = setInterval(() => {
+      if (PENDING_REQUESTS < MAX_REQUESTS_COUNT) {
+        PENDING_REQUESTS += 1;
+        clearInterval(interval);
+        resolve(config);
+      }
+    }, INTERVAL_MS);
+  }));
+
+/**
+ * Axios Response Interceptor
+ */
+APIClient.interceptors.response.use((response) => {
+  PENDING_REQUESTS = Math.max(0, PENDING_REQUESTS - 1);
+  console.debug('PENDING_REQUESTS', PENDING_REQUESTS);
+  return Promise.resolve(response);
+}, (error) => {
+  PENDING_REQUESTS = Math.max(0, PENDING_REQUESTS - 1);
+  return Promise.reject(error);
+});
+
+// const sleep = (delay) => new Promise((resolve) => {
+//   console.debug('sleep', delay);
+//     setTimeout(resolve, delay);
+//   });
+//
+// APIClient.interceptors.response.use(async (response) => {
+//     await sleep(1000);
+//     return response;
+//   }, (error) => {
+//     // Do something with response error
+//     console.error(error);
+//     return Promise.reject(error);
+//   });
 
 export default APIClient;
