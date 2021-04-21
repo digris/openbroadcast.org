ef6d225ba4ee9666e44ac660f57002622428ab08
Jonas Ohrstrom <ohrstrom@gmail.com>
2021-04-21 09:17:14 +0200

    Implemented permission based (`alibrary.play_media`) display of play / queue buttons & popup-player.

--- website/apps/player/views.py
+++ website/apps/player/views.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 import logging
 
 from django.views.generic import TemplateView
+from django.http import HttpResponseForbidden
 
 log = logging.getLogger(__name__)
 
@@ -11,6 +12,11 @@ log = logging.getLogger(__name__)
 class PlayerIndexView(TemplateView):
 
     template_name = "player/index.html"
+
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.has_perm("alibrary.play_media"):
+            return HttpResponseForbidden("Missing permissions")
+        return super(PlayerIndexView, self).dispatch(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
         context = super(PlayerIndexView, self).get_context_data(**kwargs)
