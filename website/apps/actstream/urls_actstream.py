22bf5c71e89f6d2b0ad4c86188b24957317a00d4
Jonas Ohrstrom <ohrstrom@gmail.com>
2026-02-04 16:16:20 +0100

    refactor: remove cms and other legacy shizzle

--- website/apps/actstream/urls_actstream.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url, include
-from actstream.views import ActionListView
-
-urlpatterns = [
-    url(r"^$", ActionListView.as_view(), name="actstream-action-list"),
-    url(r"^", include("actstream.urls")),
-]
--- website/apps/alibrary/views/release.py
+++
@@ -1,9 +0,0 @@
-from django.views.generic import ListView
-
-
-from ..models import Release
-
-
-class ReleaseListView(ListView):
-    model = Release
-    template_name = "alibrary/release/list.html"
